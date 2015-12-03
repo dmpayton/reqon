702295b39d3a51b281293c49f8d8cd2a14639b5b
dphaener <dphaener@gmail.com>
2015-12-03 13:55:46 -0800

    Increase test coverage
    This adds more test coverage to the library. I excluded the example
    client from the coverage reporting, because it doesn't really have any
    bearing on the coverage for the actual library code. Also, added a
    simple test for the query function in the __init__ file.

---
+++ tests/test_query.py
@@ -0,0 +1,18 @@
+import reqon
+import unittest
+import pytest
+
+class TestQuery(unittest.TestCase):
+    def test_query(self):
+        query = {
+            "$table": "foo",
+            "$query": [
+                ["$get", "foo"]
+            ]
+        }
+
+        assert str(reqon.query(query)) == "r.table('foo').get('foo')"
+
+    def test_invalid_query(self):
+        with pytest.raises(KeyError):
+            reqon.query({"$schema": "foo"})
