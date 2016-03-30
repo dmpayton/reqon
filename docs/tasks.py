c7d16c5058f097c1355ef5276474294461bb57cd
dmpayton <derek.payton@gmail.com>
2016-03-30 00:44:57 -0700

    Replace gulp with invoke for our docs build chain.
    This is a Python project, there's no reason not to use the Python
    ecosystem to build our docs.
    
    But also: Just Say No to Javascript, kids!

---
+++ docs/tasks.py
@@ -0,0 +1,27 @@
+import invoke
+import livereload
+import shutil
+
+server = livereload.Server()
+
+
+@invoke.task
+def clean():
+    shutil.rmtree('./build')
+
+
+@invoke.task(pre=[clean])
+def build():
+    invoke.run('sphinx-build ./source ./build', pty=True)
+
+
+@invoke.task(pre=[build])
+def serve():
+    server.watch('./source/*', build)
+    server.watch('./source/**/*', build)
+    server.serve(
+        root='./build',
+        host='localhost',
+        liveport=35729,
+        port=8080
+    )
